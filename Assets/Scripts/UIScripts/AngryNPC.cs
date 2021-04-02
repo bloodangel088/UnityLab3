1613648b237c53857fc2dbf17ea1bd78c5490902
bloodangel <bloodangel088@gmail.com>
2021-04-02 16:34:18 +0300

    addgame

---
+++ Assets/Scripts/UIScripts/AngryNPC.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AngryNPC : MonoBehaviour
+{
+    
+    public GameObject neponyal;
+    public GameObject aponyal;
+    public GameObject dialog;
+    public GameObject inf;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void ButtonYes()
+    {
+        dialog.SetActive(false);
+        Time.timeScale = 1;
+            neponyal.SetActive(false);
+            aponyal.SetActive(true);   
+    }
+    public void ButtonNo()
+    {
+        dialog.SetActive(false);
+        Time.timeScale = 1;
+        FindObjectOfType<ClickToMove>().gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        inf.SetActive(true);
+        neponyal.SetActive(true);
+    }
+    void OnTriggerStay(Collider collider)
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            inf.SetActive(false);
+            Time.timeScale = 0;
+            dialog.SetActive(true);
+            Debug.Log("NPC dialog started");
+        }
+    }
+    void OnTriggerExit()
+    {
+        inf.SetActive(false);
+        aponyal.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UIScripts/NPCScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class NPCScript : MonoBehaviour
+{
+    public bool endDialog;
+    public GameObject dialog;
+    
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && Time.timeScale == 0)
+            endDialog = true;
+
+
+        if (endDialog == true)
+        {
+            Time.timeScale = 1;
+            dialog.SetActive(false);
+            
+        }
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        
+
+        if (collider.tag == "Player")
+        {
+            Time.timeScale = 0;
+            dialog.SetActive(true);
+            Debug.Log("NPC dialog started");
+            
+        }
+    }
+  
+}
